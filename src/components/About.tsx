c111b9849b49505f89d24e2bbb3bafcc1fa31885
checkr <ugochiori@gmail.com>
2026-02-10 16:01:44 +0100

    menu implemented

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -89,30 +89,7 @@ const About: React.FC = () => {
 		</div>
 	 </div>
 	 
-      {/* <div className="top-grid">
-        <div className="md:col-span-3">
-          <div className="noisy" />
-          <img src="/images/abt1.png" alt="grid-image-1" />
-        </div>
-        <div className="md:col-span-6">
-          <div className="noisy" />
-          <img src="/images/abt2.png" alt="grid-image-2" />
-        </div>
-        <div className="md:col-span-3">
-          <div className="noisy" />
-          <img src="/images/abt5.png" alt="grid-image-5" />
-        </div>
-      </div>
-      <div className="bottom-grid">
-          <div className="md:col-span-8">
-          <div className="noisy" />
-          <img src="/images/abt3.png" alt="grid-image-2" />
-        </div>
-          <div className="md:col-span-4">
-          <div className="noisy" />
-          <img src="/images/abt4.png" alt="grid-image-2" />
-        </div>
-      </div> */}
+  
     </div>
   );
 };
--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -23,7 +23,7 @@ const Art: React.FC = () => {
       .to(".will-fade", {
         opacity: 0,
         stagger: 0.2,
-        ease: "power1.inOut"
+        ease: "power1.inOut",
       })
       .to(".masked-img", {
         scale: 1.3,
@@ -37,7 +37,7 @@ const Art: React.FC = () => {
   return (
     <div id="art">
       <div className="container mx-auto h-full pt-20">
-        <h2 className="will-fade"> The ART</h2>
+        <h2 className="will-fade"> L'ART</h2>
         <div className="content">
           <ul className="space-y-4 will-fade">
             {goodLists.map((feature, index) => (
@@ -66,10 +66,13 @@ const Art: React.FC = () => {
         <div className="masked-container">
           <h2 className="will-fade">Sip-worthy perfection</h2>
           <div id="masked-content" className="capitalize md:mt-12">
-            <h3>made with craft, poured with passion</h3>
+            <h3>Créé avec art, servi avec passion</h3>
+            {/* <h3>made with craft, poured with passion</h3> */}
             <p>
-              this isn't just a drink. it's a carefully crafted moment made just
-              for you
+              Ceci n’est pas qu’un simple verre. C’est un moment façonné avec
+              soin, créé spécialement pour vous.
+              {/* this isn't just a drink. it's a carefully crafted moment made just
+              for you */}
             </p>
           </div>
         </div>
@@ -79,77 +82,3 @@ const Art: React.FC = () => {
 };
 
 export default Art;
-
-
-// import gsap from 'gsap';
-// import { useMediaQuery } from 'react-responsive'
-// import { useGSAP } from '@gsap/react'
-// import { featureLists, goodLists } from "../constants/index"
-
-// const Art = () => {
-//  const isMobile = useMediaQuery({ maxWidth: 767 });
- 
-//  useGSAP(() => {
-// 	const start = isMobile ? 'top 20%' : 'top top';
-	
-// 	const maskTimeline = gsap.timeline({
-// 	 scrollTrigger: {
-// 		trigger: '#art',
-// 		start,
-// 		end: 'bottom center',
-// 		scrub: 1.5,
-// 		pin: true
-// 	 }
-// 	})
-	
-// 	maskTimeline
-// 	 .to('.will-fade', { opacity: 0, stagger: 0.2, ease: 'power1.inOut', })
-// 	 .to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '400%', duration: 1, ease: 'power1.inOut '})
-// 	 .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut'})
-//  })
- 
-//  return (
-// 	<div id="art">
-// 	 <div className="container mx-auto h-full pt-20">
-// 		<h2 className="will-fade">The ART</h2>
-		
-// 		<div className="content">
-// 		 <ul className="space-y-4 will-fade">
-// 			{goodLists.map((feature, index) => (
-// 			 <li key={index} className="flex items-center gap-2">
-// 				<img src="/images/check.png" alt="check" />
-// 				<p>{feature}</p>
-// 			 </li>
-// 			))}
-// 		 </ul>
-		 
-// 		 <div className="cocktail-img">
-// 			<img
-// 				src="/images/under-img.jpg"
-// 				alt="cocktail"
-// 				className="abs-center masked-img size-full object-contain"
-// 			/>
-// 		 </div>
-		 
-// 		 <ul className="space-y-4 will-fade">
-// 			{featureLists.map((feature, index) => (
-// 			 <li key={index} className="flex items-center justify-start gap-2">
-// 				<img src="/images/check.png" alt="check" />
-// 				<p className="md:w-fit w-60">{feature}</p>
-// 			 </li>
-// 			))}
-// 		 </ul>
-// 		</div>
-		
-// 		<div className="masked-container">
-// 		 <h2 className="will-fade">Sip-Worthy Perfection</h2>
-// 		 <div id="masked-content">
-// 			<h3>Made with Craft, Poured with Passion</h3>
-// 			<p>This isn’t just a drink. It’s a carefully crafted moment made just for you.</p>
-// 		 </div>
-// 		</div>
-// 	 </div>
-// 	</div>
-//  )
-// }
-// export default Art
